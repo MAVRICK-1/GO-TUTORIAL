bb6f46c2218c5ad6a11776b93c42cdaf704ff3b6
MAVRICK-1 <mavrickrishi@gmail.com>
2024-11-24 02:44:58 +0530

    added verdict function
    added vertict functions

---
+++ go_tutorial/veredict_function/veredict.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func add(nums ...int) int {
+	sum:=0
+	for _, num := range nums {
+		sum += num
+	}
+	return sum
+}
+
+func interface_func(val ...interface{}) {
+	for _, v := range val {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	fmt.Println(add(1,2,3,4,5)) // 15
+	fmt.Println(add(1,2,3,4,5,6,7,8,9,10)) // 55
+	fmt.Println(add(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)) // 120
+
+	interface_func(1,"hu","hello") // 1 hu hello
+	
+}
