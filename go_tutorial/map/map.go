1f940602eeca2b2e5d3152f5e9fee138183e85b9
MAVRICK-1 <mavrickrishi@gmail.com>
2024-11-23 19:49:19 +0530

    added maps

--- go_tutorial/map/map.go
+++ go_tutorial/map/map.go
@@ -4,33 +4,60 @@ import "fmt"
 
 func main() {
 
-	// Create a map
-	m := make(map[string]int)
+	m := make(map[string]string) // map[key]:value 
 
-	// Add key-value pairs
-	m["k1"] = 7
-	m["k2"] = 13
+	// m["name"] = "Priyanka"
+	// m["age"] = "25"
+	// m["city"] = "Pune"
 
-	fmt.Println("map:", m)
+	// fmt.Println(m["name"]) // Priyanka
+	// fmt.Println(m) // map[age:25 city:Pune name:Priyanka]
 
-	// Get a value for a key
-	v1 := m["k1"]
-	fmt.Println("v1: ", v1)
+	// delete(m, "age") // delete key age
 
-	// Get the length of a map
-	fmt.Println("len:", len(m))
+	// fmt.Println(m) // map[city:Pune name:Priyanka]
 
-	// Delete a key-value pair
-	delete(m, "k2")
-	fmt.Println("map:", m)
+	clear(m) // clear map
 
-	// Check if a key is present in the map
-	_, prs := m["k2"]
-	fmt.Println("prs:", prs)
+	fmt.Println(m) // map[]
 
-	// Declare and initialize a new map in a single line
-	n := map[string]int{"foo": 1, "bar": 2}
-	fmt.Println("map:", n)
+	// map with key value
+	m1 := map[string]string{
+		"name": "Rishi",
+		"age": "25",
+		"city": "Pune",
+	}
+	fmt.Println(m1) // map[age:25 city:Pune name:Rishi]
 
-	
+	_, ok := m1["name"] // check key is present or not it return 2 value 1st value is value of key and 2nd value is true or false
+
+	fmt.Println(ok) // true
+
+	value, ok1 := m1["nose"]
+
+	if ok1 {
+		fmt.Println(value)
+	} else {
+		fmt.Println("Key not present")
+	}
+
+	// map with key value
+	m2 := map[string]map[string]string{  // map[key]map[key]:value
+		"person1": {
+			"name": "Rishi",
+			"age": "25",
+			"city": "Pune",
+		},
+
+		"person2": {
+			"name": "Priyanka",
+			"age": "25",
+			"city": "Pune",
+		},
+	}
+	fmt.Println(m2) // map[person1:map[age:25 city:Pune name:Rishi] person2:map[age:25 city:Pune name:Priyanka]]
+
+	fmt.Println(m2["person1"]) // map[age:25 city:Pune name:Rishi]
+
+	fmt.Println(m2["person1"]["name"]) // Rishi
 }
